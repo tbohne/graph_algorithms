e8f42ed58caccd7d7a83cee38877961d12c0c38e
tbohne <tbohne@uos.de>
2019-04-16 17:40:56 +0200

    fixed undirected graph matrix test

--- P1/src/test/java/de/uos/inf/ko/ga/graph/UndirectedGraphMatrixTest.java
+++ P1/src/test/java/de/uos/inf/ko/ga/graph/UndirectedGraphMatrixTest.java
@@ -1,13 +1,13 @@
 package de.uos.inf.ko.ga.graph;
 
-import de.uos.inf.ko.ga.graph.Graph;
 import de.uos.inf.ko.ga.graph.impl.UndirectedGraphList;
+import de.uos.inf.ko.ga.graph.impl.UndirectedGraphMatrix;
 
 public class UndirectedGraphMatrixTest extends UndirectedGraphTest<UndirectedGraphList> {
 
 	@Override
 	protected Graph createGraph() {
-		return new UndirectedGraphList();
+		return new UndirectedGraphMatrix();
 	}
 
 }
