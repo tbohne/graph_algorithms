fdb00cb452bc375553341edb89b963e5c3da4442
tbohne <tbohne@uos.de>
2019-04-22 01:11:08 +0200

    DirectedGraphList - added doc

--- P1/src/main/java/de/uos/inf/ko/ga/graph/impl/DirectedGraphList.java
+++ P1/src/main/java/de/uos/inf/ko/ga/graph/impl/DirectedGraphList.java
@@ -6,20 +6,37 @@ import de.uos.inf.ko.ga.graph.Graph;
 
 /**
  * Implementation of a directed graph with a list representation of the edges.
+ *
+ * @author Tim Bohne
  */
 public class DirectedGraphList implements Graph {
 
     private List<Map<Integer, Double>> adjacencyList;
 
+    /**
+     * Constructor
+     */
     public DirectedGraphList() {
         this.adjacencyList = new ArrayList<>();
     }
 
+    /**
+     * Method to add the edge from start to end to the graph. Adding self-loops is not allowed.
+     * @param start start vertex
+     * @param end   end vertex
+     */
 	@Override
 	public void addEdge(int start, int end) {
         this.addEdge(start, end, 1.0);
 	}
 
+    /**
+     * Method to add the edge with the given weight to the graph from start to end.
+     * Adding self-loops is not allowed.
+     * @param start  number of start vertex
+     * @param end    number of end vertex
+     * @param weight weight of edge
+     */
 	@Override
 	public void addEdge(int start, int end, double weight) {
 	    if (start >= 0 && start < this.adjacencyList.size() && end < this.adjacencyList.size()) {
@@ -27,12 +44,19 @@ public class DirectedGraphList implements Graph {
         }
 	}
 
+    /**
+     * Method to add a vertex to the graph.
+     */
 	@Override
 	public void addVertex() {
         Map<Integer, Double> vertex = new HashMap<>();
 		this.adjacencyList.add(vertex);
 	}
 
+    /**
+     * Method to add multiple vertices to the graph.
+     * @param n number of vertices to add
+     */
 	@Override
 	public void addVertices(int n) {
 		for (int i = 0; i < n; i++) {
@@ -41,6 +65,11 @@ public class DirectedGraphList implements Graph {
         }
 	}
 
+    /**
+     * Returns all neighbors of the given vertex v (all vertices i with {i,v} in E or (i,v) or (v,i) in E).
+     * @param v vertex whose neighbors shall be returned
+     * @return List of vertices adjacent to v
+     */
 	@Override
 	public List<Integer> getNeighbors(int v) {
         List<Integer> neighbors = this.getPredecessors(v);
@@ -52,6 +81,11 @@ public class DirectedGraphList implements Graph {
         return neighbors;
 	}
 
+    /**
+     * Returns a list containing all predecessors of v.
+     * @param v vertex id
+     * @return list containing all predecessors of v
+     */
 	@Override
 	public List<Integer> getPredecessors(int v) {
         ArrayList<Integer> predecessors = new ArrayList<>();
@@ -65,6 +99,11 @@ public class DirectedGraphList implements Graph {
         return predecessors;
 	}
 
+    /**
+     * Returns a list containing all successors v.
+     * @param v vertex id
+     * @return list containing all edges starting in v
+     */
 	@Override
 	public List<Integer> getSuccessors(int v) {
         ArrayList<Integer> successors = new ArrayList<>();
@@ -74,11 +113,21 @@ public class DirectedGraphList implements Graph {
         return successors;
 	}
 
+    /**
+     * Method to get the number of vertices.
+     * @return number of vertices
+     */
 	@Override
 	public int getVertexCount() {
 		return this.adjacencyList.size();
 	}
 
+    /**
+     * Method to get the weight of the edge {start, end} / the arc (start, end).
+     * @param start start vertex of edge / arc
+     * @param end   end vertex of edge / arc
+     * @return Double.POSITIVE_INFINITY, if the edge does not exist, c_{start, end} otherwise
+     */
 	@Override
 	public double getEdgeWeight(int start, int end) {
         if (hasEdge(start, end)) {
@@ -87,6 +136,11 @@ public class DirectedGraphList implements Graph {
         return Double.POSITIVE_INFINITY;
 	}
 
+    /**
+     * Method to test whether the graph contains the edge {start, end} / the arc (start, end).
+     * @param start start vertex of the edge
+     * @param end   end vertex of the edge
+     */
 	@Override
 	public boolean hasEdge(int start, int end) {
         if (start < this.adjacencyList.size() && end < this.adjacencyList.size()) {
@@ -95,6 +149,11 @@ public class DirectedGraphList implements Graph {
         return false;
 	}
 
+    /**
+     * Method to remove an edge from the graph, defined by the vertices start and end.
+     * @param start start vertex of the edge
+     * @param end   end vertex of the edge
+     */
 	@Override
 	public void removeEdge(int start, int end) {
         if (this.hasEdge(start, end)) {
@@ -102,6 +161,9 @@ public class DirectedGraphList implements Graph {
         }
 	}
 
+    /**
+     * Method to remove the last vertex from the graph.
+     */
 	@Override
 	public void removeVertex() {
         if (!this.adjacencyList.isEmpty()) {
@@ -113,6 +175,11 @@ public class DirectedGraphList implements Graph {
         }
 	}
 
+    /**
+     * Returns whether the graph is weighted.
+     * A graph is weighted if an edge with weight different from 1.0 has been added to the graph.
+     * @return true if the graph is weighted
+     */
 	@Override
 	public boolean isWeighted() {
         for (Map m : this.adjacencyList) {
@@ -125,6 +192,10 @@ public class DirectedGraphList implements Graph {
         return false;
 	}
 
+    /**
+     * Returns whether the graph is directed.
+     * @return true if the graph is directed
+     */
 	@Override
 	public boolean isDirected() {
 		return true;
