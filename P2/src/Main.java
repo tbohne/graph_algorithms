85252dd31fb1dced5d254fa2c5a9271606178b34
tbohne <tbohne@uos.de>
2019-04-28 13:44:36 +0200

    prepared DLS

--- P2/src/Main.java
+++ P2/src/Main.java
@@ -1,9 +1,29 @@
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
 
         SlidingPuzzle puzzle = new SlidingPuzzle();
         System.out.println(puzzle);
+
+    }
+
+    public boolean depthLimitedSearch(SlidingPuzzle puzzle, int depth) {
+
+        if (puzzle.problemSolved()) { return true; }
+
+        Stack stack = new Stack<SlidingPuzzle>();
+        stack.push(puzzle);
+
+        while (!stack.isEmpty()) {
+
+            SlidingPuzzle current = (SlidingPuzzle) stack.peek();
+            SlidingPuzzle neighbor = current.getNeighbor();
+
+        }
+
+        return true;
 
     }
 
--- P2/src/SlidingPuzzle.java
+++ P2/src/SlidingPuzzle.java
@@ -7,38 +7,59 @@ public class SlidingPuzzle {
     private final int DIMENSION = 3;
 
     private int[][] board;
+    private ArrayList<SlidingPuzzle> neighbors;
+
 
     public SlidingPuzzle() {
         this.generateInitialBoard();
+        this.neighbors = new ArrayList<>();
+        this.generateNeighbors();
+    }
+
+    public void generateNeighbors() {
+
     }
 
     public int getRandomNumber(ArrayList<Integer> poolOfNumbers) {
         Collections.shuffle(poolOfNumbers);
-        System.out.println(poolOfNumbers);
         int number = poolOfNumbers.get(0);
         poolOfNumbers.remove(0);
         return number;
     }
 
     public void generateInitialBoard() {
-
         this.board = new int[DIMENSION][DIMENSION];
-
         ArrayList<Integer> numbersToBeAssigned = new ArrayList<>();
         for (int i = 1; i < DIMENSION * DIMENSION; i++) {
             numbersToBeAssigned.add(i);
         }
-
         int emptyPos = new Random().nextInt(8);
-
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
                 if (i * DIMENSION + j != emptyPos) {
                     this.board[i][j] = this.getRandomNumber(numbersToBeAssigned);
                 }
-
             }
         }
+    }
+
+    public SlidingPuzzle getNeighbor() {
+
+//        if (this.neighbors)
+
+        return new SlidingPuzzle();
+
+    }
+
+    public boolean problemSolved() {
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                if (this.board[i][j] != i * DIMENSION + j + 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public String toString() {
