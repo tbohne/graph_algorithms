bfb29c7e5cd8cdd5e61dcc8198eb3d5f481676ea
tbohne <tbohne@uos.de>
2019-04-28 13:59:27 +0200

    implemented neighborhood generation

--- P2/src/SlidingPuzzle.java
+++ P2/src/SlidingPuzzle.java
@@ -16,8 +16,68 @@ public class SlidingPuzzle {
         this.generateNeighbors();
     }
 
+    public SlidingPuzzle(SlidingPuzzle puzzle) {
+        this.board = new int[DIMENSION][DIMENSION];
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                this.board[i][j] = puzzle.board[i][j];
+            }
+        }
+        this.neighbors = new ArrayList<>();
+//        this.generateNeighbors();
+    }
+
     public void generateNeighbors() {
 
+        int xCoordEmpty = -1;
+        int yCoordEmpty = -1;
+        // find empty pos
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                if (this.board[i][j] == 0) {
+                    xCoordEmpty = j;
+                    yCoordEmpty = i;
+                }
+            }
+        }
+
+        // above case
+        if (yCoordEmpty - 1 >= 0) {
+            SlidingPuzzle nbr = new SlidingPuzzle(this);
+            nbr.board[yCoordEmpty][xCoordEmpty] = this.board[yCoordEmpty - 1][xCoordEmpty];
+            nbr.board[yCoordEmpty - 1][xCoordEmpty] = 0;
+            this.neighbors.add(nbr);
+        }
+
+        // below case
+        if (yCoordEmpty + 1 < DIMENSION) {
+            SlidingPuzzle nbr = new SlidingPuzzle(this);
+            nbr.board[yCoordEmpty][xCoordEmpty] = this.board[yCoordEmpty + 1][xCoordEmpty];
+            nbr.board[yCoordEmpty + 1][xCoordEmpty] = 0;
+            this.neighbors.add(nbr);
+        }
+
+        // left case
+        if (xCoordEmpty - 1 >= 0) {
+            SlidingPuzzle nbr = new SlidingPuzzle(this);
+            nbr.board[yCoordEmpty][xCoordEmpty] = this.board[yCoordEmpty][xCoordEmpty - 1];
+            nbr.board[yCoordEmpty][xCoordEmpty - 1] = 0;
+            this.neighbors.add(nbr);
+        }
+
+        // right case
+        if (xCoordEmpty + 1 < DIMENSION) {
+            SlidingPuzzle nbr = new SlidingPuzzle(this);
+            nbr.board[yCoordEmpty][xCoordEmpty] = this.board[yCoordEmpty][xCoordEmpty + 1];
+            nbr.board[yCoordEmpty][xCoordEmpty + 1] = 0;
+            this.neighbors.add(nbr);
+        }
+
+        for (SlidingPuzzle p : this.neighbors) {
+            System.out.println("#############");
+            System.out.println(p);
+            System.out.println("#############");
+        }
     }
 
     public int getRandomNumber(ArrayList<Integer> poolOfNumbers) {
@@ -38,6 +98,8 @@ public class SlidingPuzzle {
             for (int j = 0; j < DIMENSION; j++) {
                 if (i * DIMENSION + j != emptyPos) {
                     this.board[i][j] = this.getRandomNumber(numbersToBeAssigned);
+                } else {
+                    this.board[i][j] = 0;
                 }
             }
         }
