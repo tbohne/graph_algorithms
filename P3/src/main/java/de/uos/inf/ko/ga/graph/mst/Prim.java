1831ad7e456652e408b8538e1d14e53abd9ee980
tbohne <tbohne@uos.de>
2019-05-03 21:50:51 +0200

    refined list implementation

--- P3/src/main/java/de/uos/inf/ko/ga/graph/mst/Prim.java
+++ P3/src/main/java/de/uos/inf/ko/ga/graph/mst/Prim.java
@@ -8,14 +8,14 @@ import java.util.*;
 public class Prim {
 
 
-    public static int getCheapestNextVertex(ArrayList<Double> currentCosts, ArrayList<Integer> alreadyPartOfSpanningTree) {
+    public static int getCheapestNextVertex(double[] currentCosts, List<Integer> vertices) {
 
         double min = Integer.MAX_VALUE;
         int cheapestVertex = -1;
 
-        for (int vertex = 0; vertex < currentCosts.size(); vertex++) {
-            if (!alreadyPartOfSpanningTree.contains(vertex) && currentCosts.get(vertex) < min) {
-                min = currentCosts.get(vertex);
+        for (int vertex = 0; vertex < currentCosts.length; vertex++) {
+            if (!vertices.contains(vertex) && currentCosts[vertex] < min) {
+                min = currentCosts[vertex];
                 cheapestVertex = vertex;
             }
         }
@@ -23,15 +23,15 @@ public class Prim {
     }
 
     public static void updateCurrentCosts(
-        Graph graph, int vertexToBeAdded, ArrayList<Integer> alreadyPartOfSpanningTree, ArrayList<Double> currentCosts, ArrayList<Integer> predecessors
+        Graph graph, int vertexToBeAdded, List<Integer> vertices, double[] currentCosts, int[] predecessors
     ) {
         for (int vertex = 0; vertex < graph.getVertexCount(); vertex++) {
 
-            if (graph.hasEdge(vertexToBeAdded, vertex) && !alreadyPartOfSpanningTree.contains(vertex)
-                    && graph.getEdgeWeight(vertexToBeAdded, vertex) < currentCosts.get(vertex)) {
+            if (graph.hasEdge(vertexToBeAdded, vertex) && !vertices.contains(vertex)
+                && graph.getEdgeWeight(vertexToBeAdded, vertex) < currentCosts[vertex]) {
 
-                predecessors.set(vertex, vertexToBeAdded);
-                currentCosts.set(vertex, graph.getEdgeWeight(vertexToBeAdded, vertex));
+                predecessors[vertex] = vertexToBeAdded;
+                currentCosts[vertex] = graph.getEdgeWeight(vertexToBeAdded, vertex);
             }
         }
     }
@@ -45,34 +45,32 @@ public class Prim {
 		assert(graph != null);
 		assert(!graph.isDirected());
 
-		final Graph mst = new UndirectedGraphList();
+        int[] predecessors = new int[graph.getVertexCount()];
+        double[] currentCosts = new double[graph.getVertexCount()];
+        List<Integer> vertices = new ArrayList<>();
 
-        ArrayList<Integer> predecessors = new ArrayList<>();
-        ArrayList<Double> currentCosts = new ArrayList<>();
-        ArrayList<Integer> alreadyPartOfSpanningTree = new ArrayList<>();
-
+        // init predecessors and costs
         for (int i = 0; i < graph.getVertexCount(); i++) {
-            currentCosts.add(Double.MAX_VALUE);
-            predecessors.add(-1);
+            currentCosts[i] = Double.POSITIVE_INFINITY;
+            predecessors[i] = -1;
         }
-        currentCosts.set(0, 0.0);
+        currentCosts[0] = 0.0;
 
-        while (alreadyPartOfSpanningTree.size() != graph.getVertexCount()) {
-            int vertexToBeAdded = getCheapestNextVertex(currentCosts, alreadyPartOfSpanningTree);
-            alreadyPartOfSpanningTree.add(vertexToBeAdded);
-            updateCurrentCosts(graph, vertexToBeAdded, alreadyPartOfSpanningTree, currentCosts, predecessors);
+        while (vertices.size() < graph.getVertexCount()) {
+            int vertexToBeAdded = getCheapestNextVertex(currentCosts, vertices);
+            vertices.add(vertexToBeAdded);
+            updateCurrentCosts(graph, vertexToBeAdded, vertices, currentCosts, predecessors);
         }
 
+        Graph mst = new UndirectedGraphList();
         mst.addVertices(graph.getVertexCount());
+
         for (int i = 1; i < graph.getVertexCount(); i++) {
-
             // input graph not connected --> return empty graph
-            if (predecessors.get(i) == -1) { return new UndirectedGraphList(); }
+            if (predecessors[i] == -1) { return new UndirectedGraphList(); }
 
-//            System.out.println("edge: (" + predecessors.get(i) + ", " +  i + ")" + " - weight: " +  graph.getEdgeWeight(i, predecessors.get(i)));
-            mst.addEdge(predecessors.get(i), i, graph.getEdgeWeight(i, predecessors.get(i)));
+            mst.addEdge(predecessors[i], i, graph.getEdgeWeight(i, predecessors[i]));
         }
-
         return mst;
 	}
 
@@ -86,18 +84,18 @@ public class Prim {
         assert(!graph.isDirected());
 
         List<Edge> edges = new ArrayList<>();
-        Set<Integer> vertices = new HashSet<>();
+        List<Integer> vertices = new ArrayList<>();
         double[] currentCosts = new double[graph.getVertexCount()];
         int[] predecessors = new int[graph.getVertexCount()];
-
         PriorityQueue<Edge> heap = new PriorityQueue<>();
 
+        // init costs
         for (int i = 0; i < graph.getVertexCount(); i++) {
             currentCosts[i] = Double.POSITIVE_INFINITY;
         }
         vertices.add(0);
 
-        // add all neighbors of 0 to the heap
+        // add all neighbors of vertex 0 to the heap
         List<Integer> neighbors = graph.getNeighbors(0);
         for (int nbr : neighbors) {
             predecessors[nbr] = 0;
@@ -128,7 +126,7 @@ public class Prim {
                 }
             }
         }
-        final Graph mst = new UndirectedGraphList();
+        Graph mst = new UndirectedGraphList();
         mst.addVertices(graph.getVertexCount());
         for (Edge e : edges) {
             mst.addEdge(e.getVertexOne(), e.getVertexTwo(), e.getWeight());
