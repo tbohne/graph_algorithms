abf5646e848b575c894f057c6f06dad784095b3d
tbohne <tbohne@uos.de>
2019-05-22 16:00:44 +0200

    implemented bellman-ford algorithm

--- P5/src/main/java/de/uos/inf/ko/ga/graph/shortestpath/BellmanFord.java
+++ P5/src/main/java/de/uos/inf/ko/ga/graph/shortestpath/BellmanFord.java
@@ -18,7 +18,26 @@ public class BellmanFord {
 	 * @return Array containing the distance d[v] from the start node for each vertex v
 	 */
 	public static double[] shortestPaths(Graph graph, int start) {
-		throw new UnsupportedOperationException("method not implemented");
-	}
 
+        double[] d = new double[graph.getVertexCount()];
+
+        // init distances
+        for (int i = 0; i < graph.getVertexCount(); i++) {
+            d[i] = Double.POSITIVE_INFINITY;
+        }
+        d[start] = 0;
+
+        // compute shortest paths
+        for (int i = 1; i < graph.getVertexCount(); i++) {
+            // iterate over all edges
+            for (int src = 0; src < graph.getVertexCount(); src++) {
+                for (int dest : graph.getSuccessors(src)) {
+                    if (d[src] != Double.POSITIVE_INFINITY && d[src] + graph.getEdgeWeight(src, dest) < d[dest]) {
+                        d[dest] = d[src] + graph.getEdgeWeight(src, dest);
+                    }
+                }
+            }
+        }
+        return d;
+	}
 }
